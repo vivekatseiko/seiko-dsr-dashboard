06be3d3c69ebe2303562a8a13f9439ff9fd303f3
vivekatseiko <vivek@seiko.in>
2026-07-01 10:37:28 +0530

    Update store-performance.js

--- pages/store-performance.js
+++ pages/store-performance.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Dashboard.module.css";
 
 const formatIndianNumber = (num) => {
